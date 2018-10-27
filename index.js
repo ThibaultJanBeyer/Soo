399f0a51a24e54feebd85a837c9d21634bdef96c
tonis anton <tonis.anton18@gmail.com>
2018-10-27 10:22:55 +0300

    add window.soo

--- index.js
+++ index.js
@@ -1,39 +1,39 @@
-import { cssToDom } from "./helpers/dom.js";
-import "./node_modules/kelbas/index.js";
+import { cssToDom } from "./helpers/dom.js"
+import "./node_modules/kelbas/index.js"
 
 class Soo extends HTMLElement {
   constructor() {
-    super();
-    this.attachShadow({ mode: "open" });
+    super()
+    this.attachShadow({ mode: "open" })
   }
 
   connectedCallback() {
-    this.css && this.shadowRoot.appendChild(cssToDom(this.css()));
-    this.install.apply(this);
-    this.updateDom(this.render());
+    this.css && this.shadowRoot.appendChild(cssToDom(this.css()))
+    this.install.apply(this)
+    this.updateDom(this.render())
   }
 
   disconnectedCallback() {
-    this.uninstall();
+    this.uninstall()
   }
 
   fire(name, data) {
-    this.dispatchEvent(new CustomEvent(name, { detail: data }));
+    this.dispatchEvent(new CustomEvent(name, { detail: data }))
   }
 
   updateDom(content) {
     if (this.shadowRoot.children.length === 1) {
-      this.shadowRoot.appendChild(content.container);
+      this.shadowRoot.appendChild(content.container)
     } else {
-      this.shadowRoot.children[1].replaceWith(content.container);
+      this.shadowRoot.children[1].replaceWith(content.container)
     }
   }
 
   update() {
-    this.beforeUpdate();
-    this.beforeRender();
-    this.updateDom(this.render());
-    this.afterUpdate();
+    this.beforeUpdate()
+    this.beforeRender()
+    this.updateDom(this.render())
+    this.afterUpdate()
   }
 
   install() {}
@@ -49,4 +49,5 @@ class Soo extends HTMLElement {
   beforeRender() {}
 }
 
-export default Soo;
+window.soo = Soo
+export default Soo
