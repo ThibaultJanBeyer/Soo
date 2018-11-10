5d3ad8e21c4474e234ce16b666a1a8f0ac59252d
tonis anton <tonis.anton18@gmail.com>
2018-11-10 09:56:46 +0200

    Fix tests

--- tests/test.js
+++ tests/test.js
@@ -1,9 +1,43 @@
 const test = require("tape");
 const Soo = require("../build/soo.min.js");
-const example = require("./example.js");
+
+class Test extends Soo {
+  install() {
+    this.data = { liked: false, name: "Like me!" };
+  }
+
+  likeIt() {
+    this.data.liked = true;
+    this.update();
+  }
+
+  css() {
+    return `:host {
+              padding:5px;
+            }
+            button {
+                background:green;
+                padding:10px 5px;
+                outline:none;
+                cursor:pointer;
+            }`;
+  }
+
+  render() {
+    if (this.data.liked) {
+      return HTML`<span>Liked</span>`;
+    }
+    return HTML`<button onclick="${this.likeIt.bind(this)}">${
+      this.data.name
+      }</button>`;
+  }
+}
+
+customElements.define("like-button", Test);
 
 
 test("Should create component", t => {
+
   const element = document.createElement("like-button");
   document.body.appendChild(element);
   const component_selector = document.querySelector("like-button");
@@ -12,5 +46,3 @@ test("Should create component", t => {
   t.pass("component created correctly");
   t.end();
 });
-
-test.onFinish(window.__close__);
